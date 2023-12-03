77e3f764a60d0fcb8f8bf01414544f383c3f6f6c
felix-wallquist <felix.wallquist@piktiv.se>
2023-12-03 10:29:54 +0100

    Day 3

---
+++ Day 3/part_2.py
@@ -0,0 +1,64 @@
+import re
+
+
+def num_from_index(s, i):
+    result = re.finditer(r'\d+', s)
+    for match in result:
+        if i in range(match.span()[0], match.span()[1]):
+            return match.group()
+
+
+sum = 0
+with open("input.txt") as input:
+    lines = input.readlines()
+    for line_index in range(len(lines)):
+        for i in range(len(lines[line_index])):
+            symbol = lines[line_index][i]
+            if symbol == '*':
+                adjacent = []
+
+                # Left
+                if lines[line_index][i-1].isdigit():
+                    if num_from_index(lines[line_index], i-1):
+                        adjacent.append(int(num_from_index(lines[line_index], i - 1)))
+
+                # Right
+                if lines[line_index][i+1].isdigit():
+                    if num_from_index(lines[line_index], i + 1):
+                        adjacent.append(int(num_from_index(lines[line_index], i + 1)))
+
+                # Above
+                if line_index > 0:
+                    if not lines[line_index-1][i].isdigit():
+
+                        if lines[line_index-1][i - 1].isdigit():
+                            if num_from_index(lines[line_index-1], i - 1):
+                                adjacent.append(int(num_from_index(lines[line_index - 1], i - 1)))
+
+                        if lines[line_index-1][i + 1].isdigit():
+                            if num_from_index(lines[line_index-1], i + 1):
+                                adjacent.append(int(num_from_index(lines[line_index - 1], i + 1)))
+
+                    else:
+                        if num_from_index(lines[line_index-1], i):
+                            adjacent.append(int(num_from_index(lines[line_index - 1], i)))
+
+                # Below
+                if line_index < len(lines)-1:
+                    if not lines[line_index + 1][i].isdigit():
+
+                        if lines[line_index + 1][i - 1].isdigit():
+                            if num_from_index(lines[line_index+1], i - 1):
+                                adjacent.append(int(num_from_index(lines[line_index+1], i - 1)))
+
+                        if lines[line_index + 1][i + 1].isdigit():
+                            if num_from_index(lines[line_index+1], i + 1):
+                                adjacent.append(int(num_from_index(lines[line_index + 1], i + 1)))
+
+                    else:
+                        if num_from_index(lines[line_index + 1], i):
+                            adjacent.append(int(num_from_index(lines[line_index + 1], i)))
+
+                if len(adjacent) == 2:
+                    sum += adjacent[0] * adjacent[1]
+print(sum)
